4c88c76e23354484e337168b235393af58107dc3
luiz-avelar <luiz.avelar25@gmail.com>
2021-04-09 16:27:43 -0300

    Requirement 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,7 @@
 def study_schedule(start_time, end_time, target_time):
     """ Faça o código aqui. """
+    online_students = 0
+    for i in range(len(start_time)):
+        if start_time[i] <= target_time <= end_time[i]:
+            online_students += 1
+    return online_students
