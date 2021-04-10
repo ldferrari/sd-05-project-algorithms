ff0267c21c51a1144be96f9a5f8e16dda8bf4903
Jorge Lucas <jorge.lucas2264@gmail.com>
2021-04-10 16:33:18 -0300

    requisito 1 completo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
-def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+def study_schedule(start_time, end_time, target_time=0):
+    if target_time == 0:
+        return target_time
+    count = 0
+    i = 0
+    while i < len(start_time):
+        if end_time[i] >= target_time >= start_time[i]:
+            count += 1
+        i += 1
+    return count
