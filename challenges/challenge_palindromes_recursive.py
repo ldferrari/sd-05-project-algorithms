aa229232b174910422b889f622c172f16aa4555f
luiz-avelar <luiz.avelar25@gmail.com>
2021-04-10 12:49:21 -0300

    Requirement 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
 def is_palindrome_recursive(word, low, high):
     """ Faça o código aqui. """
+    if len(word) == 0 or word[low] != word[high]:
+        return False
+    if low > high:
+        return True
+    return is_palindrome_recursive(word, low + 1, high - 1)
+
+
+if __name__ == "__main__":
+    print(is_palindrome_recursive("SOCOS", 0, 4))
+    print(is_palindrome_recursive("AGUA", 0, 3))
