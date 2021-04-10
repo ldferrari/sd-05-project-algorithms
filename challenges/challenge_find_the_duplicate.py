da1d16362f91d524098adc380cc77e72be36251b
luiz-avelar <luiz.avelar25@gmail.com>
2021-04-10 14:20:16 -0300

    Requirement 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,13 @@
 def find_duplicate(nums):
     """ Faça o código aqui. """
+    if nums is None or type(nums) != list or len(nums) <= 1:
+        return False
+
+    nums.sort()
+    for i in range(len(nums) - 1):
+        if type(nums[i]) == str or nums[i] < 0:
+            return False
+        if nums[i] == nums[i + 1]:
+            return nums[i]
+
+    return False
