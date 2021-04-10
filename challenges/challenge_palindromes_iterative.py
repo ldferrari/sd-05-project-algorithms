19d982d9e3b02f1be9f172857462049fd3f78afc
luiz-avelar <luiz.avelar25@gmail.com>
2021-04-10 14:28:42 -0300

    Requirement 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,20 @@
 def is_palindrome_iterative(word):
     """ Faça o código aqui. """
+    if word is None or len(word) == 0:
+        return False
+
+    low, high = 0, len(word) - 1
+
+    while low < high:
+        if word[low] != word[high]:
+            return False
+        low += 1
+        high -= 1
+
+    return True
+
+
+if __name__ == "__main__":
+    print(is_palindrome_iterative("REVIVER"))
+    print(is_palindrome_iterative("AGUA"))
+    print(is_palindrome_iterative(""))
