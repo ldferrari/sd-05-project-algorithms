2fe67fcc2f0138935e4916dde63d59bbbc78ea17
César Alves <alvescesar@hotmail.com>
2021-04-09 22:41:47 -0300

    BONUS Req 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,12 @@
-def find_duplicate(nums):
-    """ Faça o código aqui. """
+from collections import Counter
+
+
+def find_duplicate(numbers):
+    duplicate = [i for i, count in Counter(numbers).items() if count > 1]
+
+    if (duplicate):
+        if (duplicate[0] < 0):
+            return False
+        return duplicate[0]
+
+    return False
