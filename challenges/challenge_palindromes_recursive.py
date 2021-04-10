b65bc3f70e5a77cdf0ffcb99ae1ff57a692fb2f6
Jorge Lucas <jorge.lucas2264@gmail.com>
2021-04-10 17:05:43 -0300

    requisito 2 completo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
-def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+def is_palindrome_recursive(word, low=0, high=False):
+    if word == '':
+        return False
+    if not high:
+        high = len(word) - 1
+    if low >= high:
+        return True
+    elif word[low] == word[high]:
+        return is_palindrome_recursive(word, low + 1, high - 1)
+    else:
+        return False
