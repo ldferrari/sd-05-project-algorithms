58b429353bf458c3a96cbfd00ac7cfc25afea1c7
luiz-avelar <luiz.avelar25@gmail.com>
2021-04-10 12:52:27 -0300

    Requirement 2 improvement

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -2,7 +2,7 @@ def is_palindrome_recursive(word, low, high):
     """ Faça o código aqui. """
     if len(word) == 0 or word[low] != word[high]:
         return False
-    if low > high:
+    if low >= high:
         return True
     return is_palindrome_recursive(word, low + 1, high - 1)
 
