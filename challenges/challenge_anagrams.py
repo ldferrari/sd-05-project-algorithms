fc6196c3eb07b88f55f7fe20e1cd2ca35d8f08f9
Felipe Vieira <flpvieira86@gmail.com>
2021-04-11 12:10:06 -0300

    anagrams

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,7 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) == len(second_string):
+            first = [letter for letter in first_string]
+            second = [letter for letter in second_string]
+            return set(first) == set(second)
+
+    return False
