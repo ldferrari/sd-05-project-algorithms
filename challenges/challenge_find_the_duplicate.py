d3dd7bf82aca88bf3079fc3ee89dec67b0ac81d2
Felipe Vieira <flpvieira86@gmail.com>
2021-04-11 12:11:15 -0300

    find_duplicates

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,10 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    nums.sort()
+
+    for index in range(len(nums) - 1):
+        if type(nums[index]) != int or nums[index] < 0:
+            return False
+        if nums[index] == nums[index + 1]:
+            return nums[index]
+
+    return False
