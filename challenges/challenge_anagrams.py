b1a1b274d7d037fcc9c6a7835c1057bb59649b95
Felipe Vieira <flpvieira86@gmail.com>
2021-04-11 12:14:15 -0300

    flake

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,7 +1,7 @@
 def is_anagram(first_string, second_string):
     if len(first_string) == len(second_string):
-            first = [letter for letter in first_string]
-            second = [letter for letter in second_string]
-            return set(first) == set(second)
+        first = [letter for letter in first_string]
+        second = [letter for letter in second_string]
+        return set(first) == set(second)
 
     return False
