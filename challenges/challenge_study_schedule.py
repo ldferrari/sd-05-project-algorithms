e25ca9ec7d729f16907d28669d4714e400109540
Giuliano Peixoto <giulianopeixoto@gmail.com>
2021-04-12 15:35:51 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    if start_time == [] or end_time == []:
+        return 0
+
+    online_students_counter = 0
+
+    for i in range(len(start_time)):
+        if start_time[i] <= target_time <= end_time[i]:
+            online_students_counter += 1
+
+    return online_students_counter
