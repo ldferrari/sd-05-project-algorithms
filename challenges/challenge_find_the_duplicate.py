6aafae9f73c8d3936f32cf43d4697644547db618
Christian Duessel <duessel@gmail.com>
2021-04-12 18:00:41 -0300

    req 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,10 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    sorted_nums = sorted(nums)
+    for index in range(len(sorted_nums) - 1):
+        if type(sorted_nums[index]) != int:
+            return False
+        elif sorted_nums[index] < 0:
+            return False
+        elif sorted_nums[index] == sorted_nums[index+1]:
+            return sorted_nums[index]
+    return False
