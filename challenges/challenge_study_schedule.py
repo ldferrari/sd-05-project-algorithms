a32e4ff6b0842912ebeac64163367b146376bd9f
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2021-04-12 14:47:20 -0300

    beginning

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,5 @@
+# algoritmo de, no máximo, complexidade O(n)
+# (ou seja tempo de execução proporcional ao tamanho das entradas)
+
 def study_schedule(start_time, end_time, target_time):
     """ Faça o código aqui. """
