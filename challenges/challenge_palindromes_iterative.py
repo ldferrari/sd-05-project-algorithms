59e930a8d5b4bfc8d0657c959c4b6a6a9d6306df
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2021-04-12 17:23:42 -0300

    req5 - felt like completing palindrome stuff

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,14 @@
 def is_palindrome_iterative(word):
     """ Faça o código aqui. """
+    if word and word == word[::-1]:
+        return True
+    return False
+
+
+print(is_palindrome_iterative("KAYAK"))
+print(is_palindrome_iterative("BLA"))
+
+
+# Without compulsary recursivity,
+# you can just compare word and its reversed version
+# https://www.askpython.com/python/array/reverse-an-array-in-python
