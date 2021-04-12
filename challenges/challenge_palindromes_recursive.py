66f1a4f193dbab7a8fe57f7a6d361e242a54f0c8
pedrohcalado <pedrocalado22@gmail.com>
2021-04-12 15:56:04 -0300

    Req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
 def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+    if word == "":
+        return False
+    if word[low] == word[high]:
+        if low >= high:
+            return True
+        return is_palindrome_recursive(word, low + 1, high - 1)
+    return False
+
+
+print(is_palindrome_recursive("ANA", 0, 2))
