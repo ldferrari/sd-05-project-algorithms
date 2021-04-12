ae034685b78eef6bec558f1ce3042e317a4a7e2b
Christian Duessel <duessel@gmail.com>
2021-04-12 15:16:04 -0300

    req 1 & req 2?

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,18 @@
+#    estudante   1  2  3  4
+#  start_time = [4, 1, 3, 2]
+#  end_time   = [4, 3, 4, 5]
+
+
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    if start_time == "" or end_time == "":
+        return 0
+
+    saida = 0
+
+    for index in range(len(start_time)):
+        if start_time[index] <= target_time and end_time[index] >= target_time:
+            saida += 1
+
+    return saida
+
+# print(study_schedule([4, 1, 3, 2], [4, 3, 4, 5], 3))
