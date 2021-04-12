76fdb070de24463109fb43d3070f3b6cf3dbdc78
Paulo-Dandrea <prog.dandrea@gmail.com>
2021-04-12 14:08:39 -0300

    reclonado

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,34 @@
+def quicksort(array, low, high):
+    if len(array) == 1:
+        return array
+
+    if low < high:
+        partition_index = partition(array, low, high)
+
+        quicksort(array, low, partition_index - 1)
+        quicksort(array, partition_index + 1, high)
+
+
+def partition(array, low, high):
+    i = low - 1
+
+    pivot = array[high]
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            array[i], array[j] = array[j], array[i]
+    array[i + 1], array[high] = array[high], array[i + 1]
+
+    return i + 1
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    string1 = list(first_string)
+    string2 = list(second_string)
+
+    first_sorted = quicksort(string1, 0, len(string1) - 1)
+    second_sorted = quicksort(string2, 0, len(string2) - 1)
+
+    return first_sorted == second_sorted
+
+# print(is_anagram('porra', 'porra'))
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+
+    students_on_target_time = 0
+    for starting_at, finishing_at in zip(start_time, end_time):
+        if (
+            # starting_at == target_time
+            # or finishing_at == target_time
+            starting_at <= target_time <= finishing_at
+        ):
+            students_on_target_time += 1
+
+    return students_on_target_time
