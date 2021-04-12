405739181d8f2e04d76448d5891a606e0908572c
Christian Duessel <duessel@gmail.com>
2021-04-12 18:16:30 -0300

    req 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,6 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if len(word) <= 1:
+        return False
+    arraia = list(word)
+    arraia_de_tras_pra_frente = arraia[::-1]
+    return arraia == arraia_de_tras_pra_frente
