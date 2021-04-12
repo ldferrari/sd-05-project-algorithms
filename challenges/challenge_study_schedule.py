0e016166b12e496a1f037fbac7394cb5d82c1776
ederusena <eder.sena@live.com>
2021-04-12 17:36:05 -0300

    chore: inicio projeto

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    if start_time == [] or target_time == 0:
+        return 0
+    for index in range(len(end_time)):
+        if target_time <= end_time[index]:
+            if start_time[index] <= target_time:
+                counter += 1
+
+    return counter
