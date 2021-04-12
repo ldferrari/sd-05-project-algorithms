b58c6018b646e496016d949c6eeef332b69e51b0
Douglas <douglaza@gmail.com>
2021-04-12 18:42:26 -0300

    req04: duplicate number

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,7 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    nums.sort()
+    for i in range(1, len(nums)):
+        if type(nums[i]) != int or nums[i] < 0:
+            return False
+        elif nums[i] == nums[i-1]:
+            return nums[i]
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,7 +1,5 @@
 def is_palindrome_recursive(word, low, high):
-    if word == '':
-        return False
-    if word[low] != word[high]:
+    if word == '' or word[low] != word[high]:
         return False
     if low < high + 1:
         return is_palindrome_recursive(word, low + 1, high - 1)
