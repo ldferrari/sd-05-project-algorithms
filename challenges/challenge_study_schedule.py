8a567feaf191a21513c1309c853d85f9c1ec0a4a
pedrohcalado <pedrocalado22@gmail.com>
2021-04-12 15:06:01 -0300

    Req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    result = 0
+    if (start_time == [] or end_time == []):
+        return 0
+    for index, item in enumerate(end_time):
+        if (start_time[index] <= target_time <= item):
+            result += 1
+    return result
+
+
+start_time = [2, 1, 2, 1, 4, 4]
+end_time = [2, 2, 3, 5, 5, 5]
+print(study_schedule(start_time, end_time, 3))
