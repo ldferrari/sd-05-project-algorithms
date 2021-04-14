411575a566bccc37872ea637c1776e97f1340d32
ederusena <eder.sena@live.com>
2021-04-14 00:16:19 -0300

    fix: Requisito 2 -espaços e ponto e virgula a toa

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,9 +1,9 @@
 def is_palindrome_recursive(palavra, low, high):
-    if  palavra == "" or palavra[low] != palavra[high] :
+    if palavra == "" or palavra[low] != palavra[high]:
         return False
     if low == high:
         return True
-    return is_palindrome_recursive(palavra, low + 1, high - 1);
+    return is_palindrome_recursive(palavra, low + 1, high - 1)
 
 
 print(is_palindrome_recursive("ANA", 0, len("ANA") - 1))
