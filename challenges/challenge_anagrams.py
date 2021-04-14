ddb62307ffa0bdddb448e44f5fbc89e576b8f2bc
Maurício <mauricio.sdjr@gmail.com>
2021-04-14 20:03:34 -0300

    flake8

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -4,18 +4,18 @@ def is_anagram(first_string, second_string):
 
     if len(first_string) != len(second_string):
         return False
-    
+
     if first_string == second_string:
         return True
 
-    first_string_char_list = []
-    second_string_char_list = []
+    first_str_char_list = []
+    second_str_char_list = []
 
     for char1 in first_string:
-        first_string_char_list.append(char1)
+        first_str_char_list.append(char1)
         for char2 in second_string:
-            second_string_char_list.append(char2)
-        if char1 in second_string_char_list and char2 in first_string_char_list:
+            second_str_char_list.append(char2)
+        if char1 in second_str_char_list and char2 in first_str_char_list:
             return True
 
     return False
--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,5 +1,5 @@
 def is_palindrome_iterative(word):
     if word == '':
         return False
-    
+
     return word == word[::-1]
