d54ca19c0377be38baccd46154a1344ba2128358
ederusena <eder.sena@live.com>
2021-04-14 00:10:35 -0300

    chore: Requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,9 @@
-def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+def is_palindrome_recursive(palavra, low, high):
+    if  palavra == "" or palavra[low] != palavra[high] :
+        return False
+    if low == high:
+        return True
+    return is_palindrome_recursive(palavra, low + 1, high - 1);
+
+
+print(is_palindrome_recursive("ANA", 0, len("ANA") - 1))
