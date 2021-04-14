5ae08a0dfbeb77823901b055a745252f474ff307
Maurício <mauricio.sdjr@gmail.com>
2021-04-14 17:35:10 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    if start_time == [] or end_time == []:
+        return 0
+
+    counter = 0
+
+    for i, elem in enumerate(end_time):
+        if elem < start_time[i]:
+            raise ValueError('the end time should occur after the start time')
+        if start_time[i] <= target_time <= elem:
+            counter += 1  
+    return counter
