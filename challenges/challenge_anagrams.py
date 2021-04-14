5ef5a02ccaa5687dce56f6e444f55fc822a4513c
ederusena <eder.sena@live.com>
2021-04-14 00:26:38 -0300

    chore: anagrama

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,20 @@
 def is_anagram(first_string, second_string):
-    print('tete')
+    if first_string =="" or second_string == "":
+        return False
+
+    lista1 = list(first_string)
+    lista2 = list(second_string)
+
+    lista1.sort()
+    lista2.sort()
+
+    pos = 0
+    anagrama = True
+
+    while pos < len(first_string) and anagrama:
+        if lista1[pos] == lista2[pos]:
+            pos = pos + 1
+        else:
+            anagrama = False
+
+    return anagrama
