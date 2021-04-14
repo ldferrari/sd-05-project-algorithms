1cdf9f589d1160b26e45db8e2c2e104a79fcd662
Maurício <mauricio.sdjr@gmail.com>
2021-04-14 19:53:42 -0300

    requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,21 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if first_string == '' or second_string == '':
+        return False
+
+    if len(first_string) != len(second_string):
+        return False
+    
+    if first_string == second_string:
+        return True
+
+    first_string_char_list = []
+    second_string_char_list = []
+
+    for char1 in first_string:
+        first_string_char_list.append(char1)
+        for char2 in second_string:
+            second_string_char_list.append(char2)
+        if char1 in second_string_char_list and char2 in first_string_char_list:
+            return True
+
+    return False
