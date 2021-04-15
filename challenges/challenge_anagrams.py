c8e55d8b530ddb08d53d08e9bf05ab7366e0f839
Larissa Palombo <larissa.palombo@tech4h.com.br>
2021-04-15 18:15:52 -0300

    anagramis

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,7 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) == len(second_string):
+        palavra_um = [letra for letra in first_string]
+        palabra_dois = [letra for letra in second_string]
+        return set(palavra_um) == set(palavra_dois)
+  
+    return False
