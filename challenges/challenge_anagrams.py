289949d03adec408c795a2a43a243290a404b7ab
Larissa Palombo <larissa.palombo@tech4h.com.br>
2021-04-15 18:22:51 -0300

    espaço em branco

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -3,5 +3,5 @@ def is_anagram(first_string, second_string):
         palavra_um = [letra for letra in first_string]
         palavra_dois = [letra for letra in second_string]
         return set(palavra_um) == set(palavra_dois)
-  
+
     return False
