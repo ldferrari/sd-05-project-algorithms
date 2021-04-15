f6641a3f0de5072ffa54beba7d07c2c3d95f7b77
virginia-duca <virginiambd.bio@gmail.com>
2021-04-15 09:20:16 -0300

    requisito 1, 2 e 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,6 @@
 def is_palindrome_iterative(word):
     """ Faça o código aqui. """
+    if len(word) == 0:
+        return False
+    new_word = word[::-1]
+    return True if new_word == word else False
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(start_time, end_time, target_time):
     """ Faça o código aqui. """
+    if len(start_time) == 0:
+        return 0
+    if target_time == 0:
+        return 0
+    counter = 0
+    for s, e in zip(start_time, end_time):
+        if s <= target_time <= e:
+            counter += 1
+
+    return counter
