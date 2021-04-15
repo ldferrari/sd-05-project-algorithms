2d7649c7de9097605ef9c15abde96e1069df5943
Larissa Palombo <larissa.palombo@tech4h.com.br>
2021-04-15 20:07:11 -0300

    ai ai aiiiiiiii ta acabanoooo

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,7 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    nums.sort()
+    for index in range(1, len(nums)):
+        if type(nums[index]) != int or nums[index] < 0:
+            return False
+        elif nums[index] == nums[index - 1]:
+            return nums[index]
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -5,6 +5,6 @@ def study_schedule(start_time, end_time, target_time):
     contador = 0
 
     for index in range(len(start_time)):
-        if start_time[index] <= target_time[index] >= target_time:
+        if start_time[index] <= target_time >= end_time[index]:
             contador += 1
     return contador
