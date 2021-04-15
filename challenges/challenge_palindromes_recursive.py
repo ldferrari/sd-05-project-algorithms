bf0f3735643ceaf13e86c32528da6aed247d05e5
Larissa Palombo <larissa.palombo@tech4h.com.br>
2021-04-15 17:53:26 -0300

    palindromeeeees

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,9 @@
 def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    if low == high or low == len(word) / 2:
+        return True
+    if word[low] == word[high]:
+        return is_palindrome_recursive(word, low + 1, high - 1)
+    else:
+        return False
