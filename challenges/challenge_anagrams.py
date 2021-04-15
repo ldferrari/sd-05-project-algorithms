b5730a9b4576be38ce4743675ca48e80763adf60
Larissa Palombo <larissa.palombo@tech4h.com.br>
2021-04-15 18:21:29 -0300

    palabraaaas

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,7 +1,7 @@
 def is_anagram(first_string, second_string):
     if len(first_string) == len(second_string):
         palavra_um = [letra for letra in first_string]
-        palabra_dois = [letra for letra in second_string]
+        palavra_dois = [letra for letra in second_string]
         return set(palavra_um) == set(palavra_dois)
   
     return False
