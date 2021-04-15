21f7ce1004d93ae4806b5c8b2aa973b6e10d4724
Larissa Palombo <larissa.palombo@tech4h.com.br>
2021-04-15 20:17:10 -0300

    só no pagodinhoooooooo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,10 +1,9 @@
 def study_schedule(start_time, end_time, target_time):
+    contador = 0
     if start_time == "" or end_time == "":
         return 0
 
-    contador = 0
-
     for index in range(len(start_time)):
-        if start_time[index] <= target_time >= end_time[index]:
+        if start_time[index] <= target_time <= end_time[index]:
             contador += 1
     return contador
