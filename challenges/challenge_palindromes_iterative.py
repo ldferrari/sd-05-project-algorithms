7d753d7b28f6fc455c0c7f2785997f381f2783e4
ferrari <luisdiasferrari@gmail.com>
2021-04-16 20:07:01 -0300

    requirement 2

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,8 @@
 def is_palindrome_iterative(word):
+    def is_palindrome_iterative(word):
     """ Faça o código aqui. """
+    for index in range((len(word)+1)//2):
+        if word[index] != word[-index-1]:
+            return False
+
+    return True
