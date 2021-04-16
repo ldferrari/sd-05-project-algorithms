6eb9f4d8df7efe5567a12ddc68055ed2fc6d8d78
pauloricardoz <trybe.przt@gmail.com>
2021-04-15 23:51:13 -0300

    anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,18 @@
+def shellSort(arr):
+    n = len(arr)
+    gap = n//2
+    while gap > 0:
+        for i in range(gap, n):
+            temp = arr[i]
+            j = i
+            while j >= gap and arr[j-gap] > temp:
+                arr[j] = arr[j-gap]
+                j -= gap
+            arr[j] = temp
+        gap //= 2
+    return arr
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+
+    return shellSort(list(first_string)) == shellSort(list(second_string))
