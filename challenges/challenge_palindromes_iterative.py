c55cb864716d5bf8698ecc2e0f4f122bf5b63f08
hleoc <informig@gmail.com>
2021-04-16 19:41:48 -0300

    Requsito 2

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,6 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    for index in range((len(word)+1)//2):
+        if word[index] != word[-index-1]:
+            return False
+    
+    return True
