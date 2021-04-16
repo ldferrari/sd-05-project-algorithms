6f9c4f0071d4ce71658ca041a4706bbedca32296
Giuliano Peixoto <giulianopeixoto@gmail.com>
2021-04-16 07:54:16 -0300

    flake

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -4,8 +4,8 @@ def is_palindrome_recursive(word, low, high):
 
     if word[low] != word[high]:
         return False
-        
+
     if low < high:
-        return is_palindrome_recursive(word, low + 1, high -1)
-    
+        return is_palindrome_recursive(word, low+1, high-1)
+
     return True
