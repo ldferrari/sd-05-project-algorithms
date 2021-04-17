027d0959ca69c33f4dd8a10eefbb7ddd0364a826
LizzardMedeiros <luis.medeiros@outlook.com.br>
2021-04-17 12:09:50 -0300

    ex. 02

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,4 @@
 def is_palindrome_recursive(word, low, high):
-    pass
+    if not word or word[low] is not word[high]:
+      return False
+    return True if low == high else is_palindrome_recursive(word, low + 1, high - 1)
