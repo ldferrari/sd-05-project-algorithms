e801970dd0a41bc25810d35c577e507194f07f22
LizzardMedeiros <luis.medeiros@outlook.com.br>
2021-04-17 12:41:54 -0300

    flake

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,7 +1,6 @@
 def is_anagram(first_string, second_string):
-  if not len(first_string) == len(second_string):
-      return False
-  f = [i for i in first_string]
-  s = [i for i in second_string]
-  return set(f) == set(s)
-
+    if not len(first_string) == len(second_string):
+        return False
+    f = [i for i in first_string]
+    s = [i for i in second_string]
+    return set(f) == set(s)
