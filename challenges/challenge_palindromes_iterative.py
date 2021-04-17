98c5a0079eb87b10f2f892215245ac5a8e7838ad
LizzardMedeiros <luis.medeiros@outlook.com.br>
2021-04-17 12:03:34 -0300

    Ex. 03

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,4 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+      return False
+    return True if word == word[::-1] else False
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,2 @@
 def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+    pass
