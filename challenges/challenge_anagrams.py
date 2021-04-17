89374207d10693dc3c7c843b5f29fb0513be8dec
LizzardMedeiros <luis.medeiros@outlook.com.br>
2021-04-17 12:39:40 -0300

    ex. 03

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,7 @@
 def is_anagram(first_string, second_string):
-    pass
+  if not len(first_string) == len(second_string):
+      return False
+  f = [i for i in first_string]
+  s = [i for i in second_string]
+  return set(f) == set(s)
+
