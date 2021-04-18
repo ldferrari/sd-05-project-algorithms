16fe0e122456d2784d4368c470479f2d87f457bb
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2021-04-18 16:48:13 -0300

    item 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,11 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+
+    invertido = ""
+    for i in range(len(word)-1, -1, -1):
+        invertido += word[i]
+
+    if (invertido == word):
+        return True
+    return False
