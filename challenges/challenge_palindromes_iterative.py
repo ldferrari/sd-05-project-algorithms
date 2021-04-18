fcbd5eec55697ec0bd00a8c3760ea60526e464bf
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2021-04-18 17:02:32 -0300

    yabadabadoo - finished

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -2,10 +2,10 @@ def is_palindrome_iterative(word):
     if not word:
         return False
 
-    invertido = ""
-    for i in range(len(word)-1, -1, -1):
-        invertido += word[i]
+    invertido = word[len(word)::-1]
 
     if (invertido == word):
         return True
     return False
+
+# https://stackoverflow.com/questions/509211/understanding-slice-notation
