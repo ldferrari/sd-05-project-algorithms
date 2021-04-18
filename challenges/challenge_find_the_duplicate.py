a649c92a336cf57154e77aa57502bfd87ce22837
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2021-04-18 19:26:35 -0300

    Passando no find duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,14 @@
+from challenges.challenge_anagrams import sorter
+
+
 def find_duplicate(nums):
     """ Faça o código aqui. """
+    if not nums or isinstance(nums, str) or len(nums) <= 1:
+        return False
+    sorted = sorter(nums)
+    for i, item in enumerate(sorted):
+        if not isinstance(item, int) or item < 0:
+            return False
+        if item == sorted[i - 1]:
+            return item
+    return False
