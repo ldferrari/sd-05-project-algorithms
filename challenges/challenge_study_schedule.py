3f59725ac13efb6917d9e454689f59e037c24eb9
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2021-04-17 22:57:02 -0300

    item 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    if (not (start_time and end_time and target_time)):
+        return 0
+
+    contador = 0
+
+    for i in range(len(start_time)):
+        hours = list(range(start_time[i], end_time[i]+1))
+        if target_time in hours:
+            contador += 1
+    return contador
