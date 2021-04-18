310b4dc15d3879364b100533804a96d027c69363
RafaAssad <rafaassadeng@gmail.com>
2021-04-18 08:10:01 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,7 @@
 def study_schedule(start_time, end_time, target_time):
     """ Faça o código aqui. """
+    online = 0
+    for i in range(len(start_time)):
+        if start_time[i] <= target_time <= end_time[i]:
+            online += 1
+    return online
