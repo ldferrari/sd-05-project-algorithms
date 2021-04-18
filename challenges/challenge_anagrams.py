157efe4914c87045ad74a520c5f0aa33bdbc27a9
lucascv <lucascv333@gmail.com>
2021-04-18 13:18:28 -0300

    add req3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,8 @@
+# Ajuda do repo da Virginia
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) == len(second_string):
+        first = [i for i in first_string]
+        second = [i for i in second_string]
+        return set(first) == set(second)
+
+    return False
