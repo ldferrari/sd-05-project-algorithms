9b0cd2f5523b50d72bba81dfa5dc3a6536b1c6e4
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2021-04-18 18:32:51 -0300

    Passando no palindromes_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,9 @@
-def is_palindrome_recursive(word, low, high):
+def is_palindrome_recursive(word=None, low=0, high=0):
     """ Faça o código aqui. """
+    if word is None or word == '':
+        return False
+    if word[low] != word[high]:
+        return False
+    if high == low:
+        return True
+    return is_palindrome_recursive(word, low + 1, high - 1)
