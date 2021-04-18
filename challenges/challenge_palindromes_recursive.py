980056de0ccdfb171417bd5101d6fb98eb796cf2
RafaAssad <rafaassadeng@gmail.com>
2021-04-18 08:24:14 -0300

    req 2 whitespace add

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -7,6 +7,6 @@ def is_palindrome_recursive(word, low, high):
         return False
 
     if low < high:
-        return is_palindrome_recursive(word, low + 1, high -1)
+        return is_palindrome_recursive(word, low + 1, high - 1)
 
     return True
