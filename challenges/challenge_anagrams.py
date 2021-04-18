9778343a13c01c8b0200d0d082979d65ef5c6640
Rodrigo Freitas <rfreitasbatista@gmail.com>
2021-04-18 15:23:42 -0300

    Identação e bug

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,7 +1,7 @@
 def is_anagram(first_string, second_string):
     if len(first_string) == len(second_string):
-            letrasDaPrimeiraString = [letra for letra in first_string]
-            letrasDaSegundaString = [letra for letra in second_string]
-            return set(letrasDaPrimeiraString) == set(letrasDaSegundaString)
+        letrasDaPrimeiraString = [letra for letra in first_string]
+        letrasDaSegundaString = [letra for letra in second_string]
+        return set(letrasDaPrimeiraString) == set(letrasDaSegundaString)
 
-        return False
+    return False
