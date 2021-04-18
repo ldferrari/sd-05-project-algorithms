7855a4aaaf30c3475756a7ce3353e2a905dc9eea
Rodrigo Freitas <rfreitasbatista@gmail.com>
2021-04-18 14:57:35 -0300

    Primeiro requisito

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    contador = 0
+    if start_time == "" or end_time == "":
+        return contador
+
+    for i in range(len(start_time)):
+        if start_time[i] <= target_time <= end_time[i]:
+            contador += 1
+
+    return contador
