1c572a127051976ce3ab295df2415c9800511aa3
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2021-04-18 18:11:19 -0300

    tentando passar no study_chedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
-def study_schedule(start_time, end_time, target_time):
+def study_schedule(start_time = [], end_time = [], target_time = None):
     """ Faça o código aqui. """
+    count = 0
+    if not target_time or not start_time or not end_time:
+        return 0
+    for idx in range(len(start_time)):
+        if start_time[idx] <= target_time <= end_time[idx]:
+            count += 1
+    return count
+    
+    
+if __name__ == '__main__':
+    print(study_schedule())
