78ee5f93449adfb2ae4c380288f0b3ed3d67760a
Rodrigo Freitas <rfreitasbatista@gmail.com>
2021-04-18 15:16:47 -0300

    Requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,7 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) == len(second_string):
+            letrasDaPrimeiraString = [letra for letra in first_string]
+            letrasDaSegundaString = [letra for letra in second_string]
+            return set(letrasDaPrimeiraString) == set(letrasDaSegundaString)
+
+        return False
