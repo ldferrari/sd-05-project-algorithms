78f4ef79a00f4cc42cc71b71598045aabae3414f
lucascv <lucascv333@gmail.com>
2021-04-18 12:53:18 -0300

    add req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,8 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    if len(start_time) == 0 or len(end_time) == 0:
+        return 0
+    counter = 0
+    for index, item in enumerate(end_time):
+        if (start_time[index] <= target_time <= item):
+            counter += 1
+    return counter
