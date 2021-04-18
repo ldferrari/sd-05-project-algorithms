d2f57331ab915471799ea4717a3f589249b7de82
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2021-04-18 18:56:26 -0300

    Passando no anagrams

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,17 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    if sorter(first_string) == sorter(second_string):
+        return True
+    return False
+
+
+def sorter(word):
+    word_to_sort = list(word)
+    if not word or word == '':
+        return []
+    first_item = word_to_sort[0]
+    less_than = sorter([letter for letter in word_to_sort[1:]
+                        if letter < first_item])
+    greater_than = sorter([letter for letter in word_to_sort[1:]
+                           if letter >= first_item])
+    return less_than + [first_item] + greater_than
