7df3bce656973566b5ed791a6bed537e637b231d
Andrevlc <andrevezi@gmail.com>
2021-04-19 19:50:18 -0300

    Requisite 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,6 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    student = 0
+    for index in range(len(start_time)):
+        if start_time[index] <= target_time and end_time[index] >= target_time:
+            student += 1
+    return student
