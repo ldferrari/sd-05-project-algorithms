f47aea79bbe0df67b0747041f073ad99df87a981
AndersonAGodoy <anderson.a.godoy@gmail.com>
2021-04-19 14:57:16 -0300

    requisito 3 feito

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,8 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+
+    if len(first_string) == len(second_string):
+        letra_primeira_string = [letra for letra in first_string]
+        letra_segunda_string = [letra for letra in second_string]
+        return set(letra_primeira_string) == set(letra_segunda_string)
+
+    return False
