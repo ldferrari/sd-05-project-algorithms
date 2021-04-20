d9fe633d2ff2fefa1d036580dc379b3be1a23f59
RenataNovais <rnovaiscs@gmail.com>
2021-04-20 00:10:28 -0300

    Requisito 3, challenge anagrams.

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,12 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    """Verifica se uma palavra é anagrama da outra. """
+
+    # Verifica que não foi passado alguma string vazia.
+    if not first_string or not second_string:
+        return False
+
+    # Verifica se o tamanho das palavras é o mesmo.
+    if len(first_string) != len(second_string):
+        return False
+
+    return set(first_string) == set(second_string)
