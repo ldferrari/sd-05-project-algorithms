8589d103422b0976d4ffe495819f3bfa3db9a09f
RenataNovais <rnovaiscs@gmail.com>
2021-04-20 00:01:14 -0300

    Requisito 2, palindromes recursive.

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
 def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+    """ Verifica se a palavra é um palindromo. """
+
+    # Retorna falso se for vazio, ou se a letra a direita é diferente
+    # da letra a esquerda no indice correspondente.
+    if not word or word[low] != word[high]:
+        return False
+
+    if low < high:
+        return is_palindrome_recursive(word, low + 1, high - 1)
+
+    return True
