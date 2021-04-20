a1ae93b304361052c626f8664e5bc70d4eedb96c
RenataNovais <rnovaiscs@gmail.com>
2021-04-19 23:39:53 -0300

    Requisito 1, study schedule.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    """ Retorna número de estudantes ativos no target_time. """
+    students = 0
+
+    if (not start_time or not end_time):
+        return 0
+
+    for [i, s] in enumerate(start_time):
+        if s <= target_time <= end_time[i]:
+            students += 1
+
+    return students
